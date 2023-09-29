a459bff2f60dab1783b0ab46a9d6d1aee5c1e0ec
Mariano Loza <marianoloza68@gmail.com>
2023-09-28 23:39:47 -0300

    added buttons

--- src/App.style.ts
+++ src/App.style.ts
@@ -86,3 +86,50 @@ export const Switcher = styled.div<Theme>`
     transition: all 0.1s linear;
 
 `
+
+export const Input = styled.div`
+  width: 100%;
+  height: 79px;
+  padding: 25px;
+  font-size: 1.5rem;
+  color: var(--white-text);
+  background-color: var(--screen-background);
+  text-align: right;
+  margin: 1.3rem 0;
+  border-radius: 0.5rem;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 100%;
+  padding: 15px 10px;
+  background-color: var(--toggle-background);
+  min-height: 290px;
+  border-radius: 0.5rem;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(60px, 65px));
+  grid-auto-rows: 40px;
+  gap: 1.2rem 0.9rem;
+  align-items: center;
+  @media screen and (min-width: 450px) {
+    grid-template-columns: repeat(auto-fill, minmax(65px, 67px));
+  }
+`;
+
+export const Button = styled(Flex)<Props>`
+  border-radius: 0.3rem;
+  padding: 10px;
+  font-size: 1.2rem;
+  opacity: 0.8;
+  cursor: pointer;
+  max-width: ${(props) => (props.gc ? "100%" : "65px")};
+  grid-column: ${(props) => (props.gc ? props.gc : "auto")};
+  background-color: ${(props) =>
+    props.bg ? props.bg : "var(--key-background-orange) "};
+  color: ${(props) =>
+    props.color ? props.color : "var(--very-dark-grayish-blue)"};
+  box-shadow: 0 3px 0
+    ${(props) => (props.bdbox ? props.bdbox : "var(--key-shadow-orange)")};
+  &:hover {
+    opacity: 1;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,96 @@
-import { useState } from 'react';
-import GlobalStyle1 from './styles/theme1';
-import GlobalStyle2 from './styles/theme2';
-import GlobalStyle3 from './styles/theme3';
-import { Container, Header, WrapperSwitch, Switch, SwitcherContainer, Switcher } from './App.style';
+import { useState } from "react";
+import GlobalStyle1 from "./styles/theme1";
+import GlobalStyle2 from "./styles/theme2";
+import GlobalStyle3 from "./styles/theme3";
+import {
+  Container,
+  Header,
+  WrapperSwitch,
+  Switch,
+  SwitcherContainer,
+  Switcher,
+  Input,
+  ButtonContainer,
+  Button,
+} from "./App.style";
 
 function App() {
-
-
   return (
-   <>
-   {<GlobalStyle1 />}
-   <Container>
-    <Header>
-      Calc
+    <>
+      {<GlobalStyle1 />}
+      <Container>
+        <Header>
+          Calc
+          <WrapperSwitch>
+            theme
+            <Switch>
+              <div>
+                <span>1</span>
+                <span>2</span>
+                <span>3</span>
+              </div>
 
-      <WrapperSwitch>
-        theme
-        <Switch>
-          <div>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-          </div>
+              <SwitcherContainer>
+                <Switcher />
+              </SwitcherContainer>
+            </Switch>
+          </WrapperSwitch>
+        </Header>
 
-          <SwitcherContainer>
-            <Switcher />
-          </SwitcherContainer>
+        <Input></Input>
 
+        <ButtonContainer>
+          <Button>7</Button>
+          <Button>8</Button>
+          <Button>9</Button>
+          <Button>
+           DEL
+          </Button>
 
-        </Switch>
+          <Button>4</Button>
+          <Button>5</Button>
+          <Button>6</Button>
+          <Button>
+            +
+          </Button>
 
-      </WrapperSwitch>
-    </Header>
-   </Container>
+          <Button>1</Button>
+          <Button>2</Button>
+          <Button>3</Button>
+          <Button>
+            -
+          </Button>
 
-   </>
-  )
+          <Button>.</Button>
+          <Button>0</Button>
+          <Button>/</Button>
+          <Button>
+            x
+          </Button>
+
+          <Button
+            gc="1/3"
+            color="var(--white)"
+            bg="var(--key-background-dark-blue)"
+            bdbox="var(--key-shadow-dark-blue)"
+          >
+            RESET
+          </Button>
+
+          <Button
+            gc="3/5"
+            color={"var(--white)"}
+            bg="var(--key-background-red)"
+            bdbox="var(--key-shadow-dark-red)"
+
+          >
+            =
+          </Button>
+
+        </ButtonContainer>
+      </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
